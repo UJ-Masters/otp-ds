cfbbe865a47e39849e3f8c0a4519e937ad080e06
mkay47 <201402344@student.uj.ac.za>
2022-08-10 23:16:15 +0200

    added swagger and security

---
+++ otp-service/src/main/java/za/ac/uj/masters/otp/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package za.ac.uj.masters.otp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/actuator/**", "/health/**",
+                        "/swagger-ui/**", "/swagger-ui/index.html",
+                        "/swagger-ui.html", "/v3/api-docs/**")
+                .permitAll()
+                .and()
+                .cors()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/send/**", "/validate/**")
+                .hasAuthority("SCOPE_write")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
+}
---
+++ otp-service/src/main/java/za/ac/uj/masters/otp/config/WebConfig.java
@@ -0,0 +1,37 @@
+package za.ac.uj.masters.otp.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+
+        RestTemplate restTemplate = builder.build();
+
+        restTemplate
+                .setMessageConverters(Arrays
+                        .asList(new StringHttpMessageConverter(),
+                                new FormHttpMessageConverter()));
+
+        return builder.build();
+    }
+}
